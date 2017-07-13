ea2069b32544a463e9a3a4c08ca05d2d46d07cb8
kevin <kevin.lambert@devoteamgcloud.com>
2017-07-13 13:05:07 +0200

    Add setup task

---
+++ tasks/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CURRENT_PATH=`pwd`
+# Create Lib directory if doesn't exist.
+if [ -d "${CURRENT_PATH}/venv" ]
+then
+    logger -s -t [INFO] "remove ${CURRENT_PATH}/venv"
+    rm -rf "${CURRENT_PATH}/venv"
+fi
+logger -s -t [INFO] "create virtual env on ${CURRENT_PATH}/venv"
+
+# Install venv.
+virtualenv -q -p python2 ${CURRENT_PATH}/venv || logger -s -t [ERROR] "Failed to create virtualenv."
+# Look for dev file.
+if [ -f "${CURRENT_PATH}/requirements/dev.txt" ]; then
+    dev_file="${CURRENT_PATH}/requirements/dev.txt"
+    ${CURRENT_PATH}/venv/bin/pip2 install -q -r ${dev_file} || logger -s -t [ERROR] "failed install ${dev_file}"
+fi
+
+logger -s -t [INFO] "Setup script ended successfully"
