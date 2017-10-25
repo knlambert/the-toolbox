34a1157863117ea6a418eccfe2c581d6fd654a3d
kevin <kevin.lambert@free.fr>
2017-10-25 14:02:25 +0200

    Better mails

--- database/test.sql
+++ database/test.sql
@@ -1,21 +1,42 @@
 
-CREATE OR REPLACE VIEW tasks_left AS
+CREATE OR REPLACE VIEW comment_notification AS
 (
     SELECT
-    T.id,
-    T.title,
-    P.id AS 'project_id',
-    P.name AS 'project_name',
-    UHT.user AS 'user_id',
-    CONCAT("projects/", P.id, "/tasks/", T.id) AS 'link'
-    FROM task T
-    JOIN user_has_task UHT
-    ON T.id = UHT.task
+    C.id AS 'COMMENT_ID',
+    T.id AS 'TASK_ID',
+    T.title AS 'TASK_TITLE',
+    P.name AS 'PROJECT_NAME',
+    P.id AS 'PROJECT_ID',
+    U.email AS 'USER_EMAIL',
+    U.id AS 'AUTHOR_ID',
+    U.name AS 'AUTHOR_NAME',
+    CONCAT("projects/", P.id, "/tasks/", T.id) AS 'TASK_LINK'
+    FROM comment C
+    JOIN task T
+    ON C.task = T.id
     JOIN task_list TL
     ON T.task_list = TL.id
     JOIN project P
     ON TL.project = P.id
-    WHERE T.completed = 0
-    ORDER BY T.created_at
-)
-;
+    JOIN user_has_task UHT
+    ON T.id = UHT.task
+    JOIN user U
+    ON UHT.user = U.id
+    JOIN user U2
+    ON C.author = U2.id
+);
+
+-- CREATE OR REPLACE VIEW assignement_notification AS 
+-- (
+--     SELECT
+--     T.id AS 'TASK_ID',
+--     T.title AS 'TASK_TITLE',
+--     P.name AS 'PROJECT_NAME',
+--     P.id AS 'PROJECT_ID',
+--     CONCAT("projects/", P.id, "/tasks/", T.id) AS 'TASK_LINK'
+--     FROM task T
+--     JOIN task_list TL
+--     ON T.task_list = TL.id
+--     JOIN project P
+--     ON TL.project = P.id
+-- );
