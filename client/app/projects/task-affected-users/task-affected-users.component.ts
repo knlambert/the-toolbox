f1d40669b5b873e65d964f3b0f3b3116f8be64b3
Kévin LAMBERT <kevin.lambert@free.fr>
2017-10-31 07:48:28 +0100

    Get difference working. Should optimize

--- client/app/projects/task-affected-users/task-affected-users.component.ts
+++ client/app/projects/task-affected-users/task-affected-users.component.ts
@@ -15,13 +15,14 @@ export class TaskAffectedUsersComponent implements OnInit {
     @Input() availableUsers: Array<object>;
     @Input() locked: boolean;
     @Input() set savedAffectedUsers(users){
-        this._affectedUsers = users;
+        this._savedAffectedUsers = users;
+        users.forEach(user => {
+            this.addTaskAffectation(user['email']);
+        });
     }
     
     private _affectedUsers: Array<object> = [];
-
-    private _toAdd: Array<object> = [];
-    private _toRemove: Array<object> = [];
+    private _savedAffectedUsers: Array<object> = [];
 
     ngOnInit(){ }
 
@@ -36,15 +37,47 @@ export class TaskAffectedUsersComponent implements OnInit {
 
     private deleteTaskAffectation(email: string){
         let index = this.findIndexByEmail(this._affectedUsers, email);
-        this._toRemove.push(this._affectedUsers[index]);
         this.availableUsers.push(this._affectedUsers[index]);
         this._affectedUsers.splice(index, 1);
     };
 
-    private onMemberSearched(email: string){
+    private addTaskAffectation(email: string){
         let index = this.findIndexByEmail(this.availableUsers, email);
-        this._toAdd.push(this.availableUsers[index]);
-        this._affectedUsers.push(this._affectedUsers[index]);
+        this._affectedUsers.push(this.availableUsers[index]);
         this.availableUsers.splice(index, 1);
     };
+
+    public getDifferences(){
+        console.log(this._affectedUsers);
+        let toAdd = this._affectedUsers.filter((toSaveUser) => {
+            let isInList = false;
+            this._savedAffectedUsers.forEach((savedUser) => {
+                if(savedUser['email'] === toSaveUser['email']){
+                    isInList = true;
+                }
+            });
+            return !isInList;
+        });
+
+        let toRemove = [];
+        this._savedAffectedUsers.forEach((savedUser) => {
+            let isInList = false;
+            this._affectedUsers.forEach((toSaveUser) => {
+                if(savedUser['email'] === toSaveUser['email']){
+                    isInList = true;
+                }
+            });
+            if(!isInList){
+                toRemove.push(savedUser)
+            }
+        })
+
+        this._affectedUsers.forEach((user) => {
+            
+        });
+        console.log({
+            "toAdd": toAdd,
+            "toRemove": toRemove
+        });
+    }
 }
